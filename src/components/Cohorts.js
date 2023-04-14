60a1bbbcfbb3d3dd61cafb815bd64e10ad89b5e9
miluche <menjokipla@gmail.com>
2023-04-14 15:50:47 +0300

    Added the sidebar section with their respective labels

--- src/components/Cohorts.js
+++ src/components/Cohorts.js
@@ -24,32 +24,43 @@ const Cohorts = () => {
   };
 
   return (
-    <div>
-      <h1>COHORTS</h1>
-      <button onClick={handleShowForm}>Add Cohort</button>
-      {showForm && <CohortForm onClose={handleCloseForm} onAdd={handleAddCohort} />}
-      <table>
-        <thead>
-          <tr>
-            <th>Name</th>
-            <th>Course</th>
-            <th>Number of Students</th>
-            <th>Action</th>
-          </tr>
-        </thead>
-        <tbody>
-          {cohorts.map((cohort, index) => (
-            <tr key={index}>
-              <td>{cohort.name}</td>
-              <td>{cohort.course}</td>
-              <td>{cohort.numberOfStudents}</td>
-              <td>
-                <button onClick={() => handleDeleteCohort(index)}>Delete</button>
-              </td>
+    <div className="container">
+      <div className="sidebar">
+        <ul>
+          <li><a href="#">Messages</a></li>
+          <li><a href="#">Departments</a></li>
+          <li><a href="#">Projects</a></li>
+          <li><a href="#">Collaborations</a></li>
+          <li><a href="#">Dashboard</a></li>
+        </ul>
+      </div>
+      <div className="main">
+        <h1>COHORTS</h1>
+        <button onClick={handleShowForm}>Add Cohort</button>
+        {showForm && <CohortForm onClose={handleCloseForm} onAdd={handleAddCohort} />}
+        <table>
+          <thead>
+            <tr>
+              <th>Name</th>
+              <th>Course</th>
+              <th>Number of Students</th>
+              <th>Action</th>
             </tr>
-          ))}
-        </tbody>
-      </table>
+          </thead>
+          <tbody>
+            {cohorts.map((cohort, index) => (
+              <tr key={index}>
+                <td>{cohort.name}</td>
+                <td>{cohort.course}</td>
+                <td>{cohort.numberOfStudents}</td>
+                <td>
+                  <button className="delete-button" onClick={() => handleDeleteCohort(index)}>Delete</button>
+                </td>
+              </tr>
+            ))}
+          </tbody>
+        </table>
+      </div>
     </div>
   );
 };
