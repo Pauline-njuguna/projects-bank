c8571bff6e04f59b5ccf60135efb08ad5ca74973
joanne muthoni <joanne.mumbi@student.moringaschool.com>
2023-04-17 15:52:57 +0300

    adding the course section

--- src/App.js
+++ src/App.js
@@ -3,12 +3,14 @@ import React from "react";
  import { BrowserRouter,  Routes, Route } from "react-router-dom";
 import Login from "./components/Login/login";
 import Header from "./components/student home page/Header";
+import Sidebar from "./components/student home page/Sidebar";
 function App() {
   return (
     <BrowserRouter>
       <Routes>
         <Route path='login' element={<Login/>}/>
          <Route path="/" element={<Header/>}>
+         <Route path="sidebar" element={<Sidebar/>}/>
         </Route>
       </Routes>
     </BrowserRouter>
--- src/components/student home page/Header.js
+++ src/components/student home page/Header.js
@@ -1,22 +1,30 @@
 import React from 'react'
 import './Header.css'
+import Sidebar from './Sidebar'
+import Course from './Course'
 function Header() {
   return (
-    <div className='header'>
-            <img src='https://o.remove.bg/downloads/56ab9f75-f768-4336-b1e6-52ad53ef7b0b/b0d7e1b5aa7fabb42bc1d622a1bedbc5--library-logo-technology-logo-removebg-preview.png' alt='logo'/>
-            <h2>Pay-Bank</h2>
+    <>
+        <div className='header'>
+                    <img src='https://o.remove.bg/downloads/56ab9f75-f768-4336-b1e6-52ad53ef7b0b/b0d7e1b5aa7fabb42bc1d622a1bedbc5--library-logo-technology-logo-removebg-preview.png' alt='logo'/>
+                    <h2>Pay-Bank</h2>
 
-        <div className='header-nav'>
-            <span className='option-one'> Home</span>
-            <span className='option-one'>About us</span>
-            <span className='option-one'>Contact us</span>
-        </div>
+                <div className='header-nav'>
+                    <span className='option-one'> Home</span> 
+                    <span className='option-one'>About us</span>
+                    <span className='option-one'>Contact us</span>
+                </div>
 
-        <div className='student-logout'>
-            <button className='logout'>logout</button>
-        </div>
+                <div className='student-logout'>
+                    <button className='logout'>logout</button>
+                </div>
 
-    </div>
+            </div>
+
+            <Sidebar/>
+            <Course/>
+    </>
+    
   )
 }
 
