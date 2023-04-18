43accfa48a71a2dac59584080c0e9363fef65b83
miluche <menjokipla@gmail.com>
2023-04-18 16:00:58 +0300

    Added styling to Cohort form

--- src/App.js
+++ src/App.js
@@ -28,13 +28,13 @@
 // import './App.css';
 import React from "react";
 // import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
-//import Login from "./components/Login/login";
-import Cohorts from "./components/Cohorts/Cohorts";
+import Login from "./components/Login/login";
+//import Cohorts from "./components/Cohorts/Cohorts";
 
 function App() {
   return (
     <>
-   <Cohorts/>
+   <Loging/>
     </>
   );
 }
