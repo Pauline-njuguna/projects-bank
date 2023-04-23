2d34e4d70147a252cd8ed35fc7d72fbe6e2eb827
joanne muthoni <joanne.mumbi@student.moringaschool.com>
2023-04-23 14:19:26 +0300

    deleted some files

--- src/App.js
+++ src/App.js
@@ -1,16 +1,13 @@
 // import './App.css';
 import React from "react";
 import { Routes, Route } from "react-router-dom";
-import Login from "./components/Login/login";
-import Header from "./components/student home page/Header";
+
 import Contact from "./components/contactpage/Contact";
 
 function App() {
   return (
     
       <Routes>
-        <Route path='/login' element={<Login/>}/>
-         <Route path="/" element={<Header/>}/>
          <Route path="/contact" element={<Contact/>}/>
       </Routes>
     
